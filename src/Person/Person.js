de60275d8cd56a7d4cf16fbdf60956f136e25207
Morton358 <vovankovalchuk358@gmail.com>
2017-12-09 14:50:08 +0100

    examples according lessons

---
+++ src/Person/Person.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './Person.css';
+
+const person = (props) => {
+
+  return(
+    <div className="Person">
+      <p onClick={props.click}>I`m {props.name} and I`m {props.age} years old!</p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.chenged} value={props.name} />
+    </div>
+  )
+}
+
+export default person;
+
+// import React, { Component } from 'react';
+//
+// class Person extends Component {
+//   constructor(props){
+//     super(props)
+//   }
+//
+//   changeParagraph = () => {
+//     return this.props.click()
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <p onClick={this.changeParagraph}>I`m {this.props.name} and I`m {this.props.age} years old!</p>
+//         <p>{this.props.children}</p>
+//       </div>
+//     );
+//   }
+//
+// }
+//
+// export default Person;
