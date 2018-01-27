56a921470f2c862c1a737cff3f08f40a5be354e7
Morton358 <vovankovalchuk358@gmail.com>
2018-01-28 00:57:32 +0100

    Changing Persons and Person component from stateless to statefull component

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
-    return (
-        <Person
-            name={person.name}
-            key={person.id}
-            age={person.age}
-            click={() => {
-                props.clicked(index);
-            }}
-            chenged={event => {
-                props.changed(event, person.id);
-            }}
-        />
-    );
-});
-
-export default persons;
+class Persons extends Component {
+    render() {
+        return this.props.persons.map((person, index) => {
+            return (
+                <Person
+                    name={person.name}
+                    key={person.id}
+                    age={person.age}
+                    click={() => {
+                        this.props.clicked(index);
+                    }}
+                    chenged={event => {
+                        this.props.changed(event, person.id);
+                    }}
+                />
+            );
+        });
+    }
+}
+export default Persons;
