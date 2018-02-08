c4cf59d96f0cc69bf6450c8be36f9c395d486489
Morton358 <vovankovalchuk358@gmail.com>
2018-02-08 12:12:56 +0100

    created communication between fake server and application GET for Post and FullPost and POST for NewPosr

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,21 +1,65 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false,
+        errorMsg: {}
+    };
+    componentDidMount() {
+        axios
+            .get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    };
+                });
+                this.setState({ posts: updatedPosts });
+                // console.log(response);
+            })
+            .catch(error => {
+                this.setState({ error: true, errorMsg: error })
+                console.log(this.state.errorMsg);
+            });
+    }
+
+    handlerSelectPost(id) {
+        this.setState({ selectedPostId: id });
+    }
+    render() {
+        let posts = (
+            <h2 style={{ textAlign: 'center', color: 'red' }}>
+                Something goes wrong.
+            </h2>
+        );
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return (
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => {
+                            this.handlerSelectPost(post.id);
+                        }}
+                    />
+                );
+            });
+        }
         return (
             <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
+                <section className="Posts">{posts}</section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-type'] = 'application-json';
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
